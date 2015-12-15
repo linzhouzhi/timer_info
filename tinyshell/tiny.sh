6f8b1f4c831a715653508c8c5f43c0b75bab03d2
linzhouzhi <linzhouzhi@qq.com>
2015-12-15 17:46:58 +0800

    fd

--- tinyshell/tiny.sh
+++ tinyshell/tiny.sh
@@ -21,7 +21,7 @@ COUNT=50
 `/usr/bin/diff -y --left-column cname.txt tmp.txt | /bin/grep  "<" | /usr/bin/awk '{print $1}' | /bin/grep 'g$' > t.txt`
 `/bin/cp t.txt tmp.txt`
 #读取需要压缩的文件
-list=`/bin/cat /home/lzz/tinyshell/tmp.txt`
+list=`/bin/cat /phpstudy/www/swoole/tinyshell/tmp.txt`
 
 #查询可以使用的app key
 sql="select app_key,times,month from tinypng"
@@ -45,13 +45,24 @@ do
 			if [ $count -lt $COUNT ]
 			then
 				file=$dir$file_name
-				echo "curl --insecure --user api:$app_key --data-binary @$file -i https://api.tinify.com/shrink"
-				url_con=`curl --insecure --retry 3 --connect-timeout 10 -m 300 --user api:$app_key --data-binary @$file -i https://api.tinify.com/shrink`
-				echo $url_con
+				#调用压缩文件接口
+				echo "curl --insecure --retry 4 --connect-timeout 10 -m 300 --user api:$app_key --data-binary @$file -i http://"
+				url_con=`/usr/bin/curl --insecure --retry 4 --connect-timeout 10 -m 300 --user api:$app_key --data-binary @$file -i https://api.tinify.com/shrink`
+				#获取压缩文件的地址
 				tmp=${url_con#*\"url\":\"}
 				url=${tmp%\"\}\}*}
-				`wget -t 3 -T 30 -c --no-check-certificate $url -O $tiny$file_name`
+				echo "url:$url"
+				#建立相应的文件夹
+				tiny_file=$tiny$file_name
+				/bin/mkdir -p ${tiny_file%/*} | /bin/grep -v "[\.][pj][np]g$"
+				#下载文件
+				echo "wget -t 4 -T 10 -c --no-check-certificate $url -O $tiny_file"
+				`/usr/bin/wget -t 4 -T 10 -c --no-check-certificate $url -O $tiny_file --user-agent="Mozilla/5.0 (X11;U;Linux i686;en-US;rv:1.9.0.3) Geco/2008092416 Firefox/3.0.3"`
+				if [ $(stat -c %s $tiny_file) -eq 0 ];then
+					`/usr/bin/wget -t 1 -T 10 -c --no-check-certificate $url -O $tiny_file`
+				fi
 				count=`expr $count + 1`
+				#更新该接口的使用次数
 				sql="update tinypng set times=times+1 where app_key='$app_key'"
 				/phpstudy/mysql/bin/mysql -h${HOSTNAME}  -P${PORT}  -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${sql}"
 			else
@@ -63,3 +74,16 @@ do
 	fi
 done
 
+#判断哪些文件压缩后为空，说明该文有错就删除
+/bin/cat tmp.txt | while read tmp_file
+do
+	file=$tiny$tmp_file
+	if [ $(stat -c %s $file) -eq 0 ];then
+		/bin/rm -f $file
+	fi
+done
+
+#将压缩后的文件拷贝到目标文件夹中，如果目标文件夹的文件比较新则不替换
+`/bin/cp -ur $tiny* $dir`
+#将tiny_imgs 中的文件删除
+#`/bin/rm -rf $tiny*`
